9eef436521b67dd4b0195baab00b28c4bfbf10ad
Crosshair <bertold95pk@gmail.com>
2022-02-28 00:27:58 +0100

    v 1.1

--- app/Http/Controllers/ProductController2.php
+++ app/Http/Controllers/ProductController2.php
@@ -66,4 +66,15 @@ class ProductController2 extends Controller
     {
         return Product::destroy($id);
     }
+
+    /**
+     * Search for a name
+     *
+     * @param  str  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function search($name)
+    {
+        return Product::where('name', 'like', '%'.$name.'%')->get();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController2;
+use App\Http\Controllers\AuthController2;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,10 +16,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('products', ProductController2::class);
+// Route::resource('products', ProductController2::class);
 
-// Route::get('/products', [ProductController2::class, 'index']);
-// Route::post('/products', [ProductController::class, 'store']);
+// Public routes ↓↓
+Route::post('/register', [AuhController::class, 'register']);
+Route::get('/products', [ProductController2::class, 'index']);
+Route::get('/products/{id}', [ProductController2::class, 'show']);
+Route::get('/products/search/{name}', [ProductController2::class, 'search']);
+
+// Protected routes ↓↓
+Route::group(['middleware' => ['auth:sanctum']], function () {
+Route::post('/products', [ProductController::class, 'store']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
